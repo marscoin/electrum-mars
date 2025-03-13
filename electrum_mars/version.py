a192f00c6fcee75df5013aca0139bbc723dfcd7f
Lennart Lopin <lennart@bytefederal.com>
2025-03-13 17:09:10 -0400

    chore: version bump

--- electrum_mars/version.py
+++ electrum_mars/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '28.1'     # version of the client package
-APK_VERSION = '28.1.1.0'        # read by buildozer.spec
+ELECTRUM_VERSION = '28.2'     # version of the client package
+APK_VERSION = '28.2.1.0'        # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
